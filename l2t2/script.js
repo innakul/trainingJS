81ef7662314d6f4c69d2cb3dd94e9a77a81d3c74
innakul <38705060+innakul@users.noreply.github.com>
2018-05-02 18:07:22 +0300

    Update script.js

--- l2t2/script.js
+++ l2t2/script.js
@@ -1,4 +1,4 @@
-var arr = [m,a,m,a];
+var arr = [1,1,1.5,9,-10,6,2,9,9,3,5,-3,3];
 
 var res_arr = [];
 
@@ -15,5 +15,3 @@ function repit(item, i, arr) {
 }
 
 console.log(res_arr);
-
-1,1,1.5,9,-10,6,2,9,9,3,5,-3,3
